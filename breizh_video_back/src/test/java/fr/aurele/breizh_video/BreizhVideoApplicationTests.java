ca5649c5fe5fb57c19476ee8f123d8fd59ce860f
aurele3000 <aurele.hurtis@gmail.com>
2021-01-26 12:14:18 +0100

    midi 26-01

---
+++ breizh_video_back/src/test/java/fr/aurele/breizh_video/BreizhVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.aurele.breizh_video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreizhVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
